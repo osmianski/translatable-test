424a1a91d866d90f3fc3873a43fae2f547b19a4b
Vlad Osmianski <vlaosm@gmail.com>
2023-03-22 16:23:17 +0200

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    use Translatable;
+
+    public array $translatedAttributes = ['title', 'content'];
+    protected $fillable = ['author'];
+}
